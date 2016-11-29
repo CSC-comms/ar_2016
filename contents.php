12a0fe83154f283d20a89b6875127da1cd6e8dbd
rabbit-bones <josmc82@gmail.com>
2016-11-29 16:24:19 +1100

    adding new cover page

---
+++ contents.php
@@ -0,0 +1,82 @@
+<?php // This is where you add the page title, it also displays in the level 1 heading below
+	$title = 'Contents'; 
+	///////////////////////////////////////////////////
+	include 'includes/head.inc.top.php'; 
+	?>
+    	<div class="container">
+        	<div class="row">
+   		<div class="content" id="startContent">
+			<h1><a id="top" title="Top of the page" name="top"></a><?php echo $title ?></h1>
+        <ul>
+            <li><a href="copyright.php">Copyright statement</a></li>
+  			<li><a href="letter-of-transmittal.php">Letter of transmittal</a></li>
+            <li><a href="readers-guide.php">Reader's guide</a></li>
+		</ul> 
+            <h2 class="notopmargin">Introduction</h2>
+		<ul>
+			<li><a href="sections/introduction/about-csc.php">About CSC</a></li>
+            <li><a href="sections/introduction/about-csc-schemes.php">About CSC's schemes</a></li>
+            <li><a href="sections/introduction/about-csc-members.php">About CSC's members</a></li>
+         </ul>
+            <h2>Performance</h2>
+         <ul>
+            <li><a href="sections/performance/report-from-the-chair.php">Report from the Chair</a></li>
+            <li><a href="sections/performance/csc-major-achievment.php">CSC's major achievements</a></li>
+            <li><a href="sections/performance/csc-annual-performance.php">CSC's Annual Performance Statement</a></li>
+         </ul>
+            <h2>CSC Board</h2>
+         <ul>
+            <li><a href="sections/csc-board/csc-board.php">CSC Board</a></li>
+            <li><a href="sections/csc-board/board-committees.php">Board committees</a></li>
+         </ul>
+            <h2>Governance</h2>
+         <ul>
+            <li><a href="sections/governance/introduction.php">Introduction </a></li>
+         </ul>
+            <h2>Investments</h2>
+         <ul>
+            <li><a href="sections/investments/Introduction.php">Introduction</a></li>
+            <li><a href="sections/investments/how-performance-affects.php">How performance affects a member's benefit</a></li>
+            <li><a href="sections/investments/investment-approach.php">Investment approach</a></li>
+            <li><a href="sections/investments/investment-strategy.php">Investment strategy</a></li>
+            <li><a href="sections/investments/investment-governance.php">Investment governance</a></li>
+            <li><a href="sections/investments/environmental-factors.php">environmental, social and governance factors</a></li>
+            <li><a href="sections/investments/investment-options.php">Investment options</a></li>
+         </ul>
+            <h2>Super schemes</h2>
+         <ul>
+            <li><a href="sections/super-schemes/Introduction.php">Introduction</a></li>
+            <li><a href="sections/super-schemes/css.php">CSS</a></li>
+            <li><a href="sections/super-schemes/pss.php">PSS</a></li>
+            <li><a href="sections/super-schemes/militarysuper.php">MilitarySuper</a></li>
+            <li><a href="sections/super-schemes/pssap.php">PSSap</a></li>
+            <li><a href="sections/super-schemes/1922scheme.php">1922 scheme</a></li>
+            <li><a href="sections/super-schemes/DFRB-DFRDB-DFSPB.php">DFRB, DFRDB and DFSPB</a></li>
+            <li><a href="sections/super-schemes/PNG.php">PNG</a></li>
+         </ul>
+            <h2>Financial statements</h2>
+         <ul>
+            <li><a href="sections/financial-statements/Financial Statements CSS/financial-statements-css.php">CSS financial statements</a></li>
+            <li><a href="sections/financial-statements/Financial Statements PSS/financial-statement-pss.php">PSS financial statements</a></li>
+            <li><a href="sections/financial-statements/Financial Statements MS/financial-statement-ms.php">MilitarySuper financial statements</a></li>
+            <li><a href="sections/financial-statements/Financial Statements PSSap/financial-statement-pssap.php">PSSap financial statements</a></li>
+            <li><a href="sections/financial-statements/Financial Statements CSC/index.php">CSC financial statements</a></li>
+         </ul>
+            <h2>Appendices</h2>
+         <ul>
+            <li><a href="sections/appendices/appendix1.php">Appendix 1 - Changes to CSC’s governing legislation & new military super scheme legislation</a></li>
+            <li><a href="sections/appendices/glossary.php">Glossary</a></li>
+          </ul>
+            <h2>Report requirements</h2>
+          <ul>
+            <li><a href="sections/report-requirements/report-requirements.php">Report requirements</a></li>
+          </ul>
+    
+	<ul class="bottomnav">
+    <li><a href="#topnav" class="top">Top</a></li><li><a href="copyright.php" class="next">Next &#8211; Copyright statement </a></li>
+	</ul>
+</div>
+</div>
+</div>
+        
+<?php include 'includes/footer.inc.top.php'; ?>

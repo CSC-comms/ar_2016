6687c70519e224f8682afc56c6b5b53b8c1f11ba
rabbit-bones <josmc82@gmail.com>
2016-11-30 09:46:03 +1100

    replacing in page side nav with PHP include

--- sections/financial-statements/Financial Statements CSC/note_1.php
+++ sections/financial-statements/Financial Statements CSC/note_1.php
@@ -6,29 +6,8 @@
     
     <div class="container">
 		<div class="row">
-			<div class="col-xs-3 sidenav omega" id="sidenav">
-				<p class="sectiontitle">CSC financial statement</p>
-				<h2><a id="top" title="Top of the page" name="top"></a>In this section</h2>
-				<ul>
-					<li><a href="note_1.php">1. Overview</a></li>
-								  <li><a href="note_2.php">2. Expenses</a></li>
-								  <li><a href="note_3.php">3. Own-Source Revenue</a></li>
-								  <li><a href="note_4.php">4. Financial Assets</a></li>
-								  <li><a href="note_5.php">5. Non-Financial Assets</a></li>
-								  <li><a href="note_6.php">6. Payables</a></li>
-								  <li><a href="note_7.php">7. Provisions</a></li>
-								  <li><a href="note_8.php">8. Cash Flow Reconciliation</a></li>
-								  <li><a href="note_9.php">9. Appropriations</a></li>
-								  <li><a href="note_10.php">10. Special Accounts</a></li>
-								  <li><a href="note_11.php">11. Senior Management Personnel Remuneration</a></li>
-								  <li><a href="note_12.php">12. Contingent Assets and Liabilities</a></li>
-								  <li><a href="note_13.php">13. Financial Instruments</a></li>
-								  <li><a href="note_14.php">14. Fair Value Measurements</a></li>
-								  <li><a href="note_15.php">15. Assets Held in Trust</a></li>
-								  <li><a href="note_16.php">16. Restructuring</a></li>
-								  <li><a href="note_17.php">17. Reporting of Outcomes</a></li>
-				</ul>
-			</div>
+			
+			<?php include 'side-nav-notes.php' ?>
 				
 			<div class="col-xs-8 content" id="startContent">
 				<h1><?php echo $title ?></h1>

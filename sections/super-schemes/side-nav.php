7c1f6eab199982c2666141f6a5a9520cef8f0e7b
rabbit-bones <josmc82@gmail.com>
2016-11-30 13:44:29 +1100

    replacing in page side nav with php include

---
+++ sections/super-schemes/side-nav.php
@@ -0,0 +1,14 @@
+	<div class="col-xs-3 sidenav omega" id="sidenav">
+		<p class="sectiontitle">Super schemes</p>
+		<h2><a id="top" title="Top of the page" name="top"></a>In this section</h2>
+		<ul>
+			<li><a title="Super schemes" href="Introduction.php">Super schemes</a></li>
+			<li><a title="CSS" href="css.php">CSS</a></li>
+			<li><a title="PSS" href="pss.php">PSS</a></li>
+			<li><a title="MilitarySuper" href="militarysuper.php">MilitarySuper</a></li>
+			<li><a title="PSSap" href="pssap.php">PSSap</a></li>
+			<li><a title="1922 scheme" href="1922scheme.php">1922 scheme</a></li>
+			<li><a title="DFRB, DFRDB and DFSPB" href="DFRB-DFRDB-DFSPB.php">DFRB, DFRDB and DFSPB</a></li>
+			<li><a title="PNG" href="PNG.php">PNG</a></li>
+		</ul>
+	</div>

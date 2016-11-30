fd851fa6db9da7ce1653b9130fb7197cd3f8c4fd
rabbit-bones <josmc82@gmail.com>
2016-11-30 14:52:44 +1100

    replacing in page side nav with php include

---
+++ sections/financial-statements/Financial Statements PSSap/side-nav-notes.php
@@ -0,0 +1,25 @@
+	<div class="col-xs-3 sidenav omega" id="sidenav">
+		<p class="sectiontitle"><a href="financial-statement-pssap.php">PSSap financial statement</a></p>
+		<h2><a id="top" title="Top of the page" name="top"></a>In this section</h2>
+		<ul>
+			<li><a href="note_1.php">1. description of the plan</a></li>
+			<li><a href="note_2.php">2. basis of preparation</a></li>
+			<li><a href="note_3.php">3. summary of principle accounting policies</a></li>
+			<li><a href="note_4.php">4. investments</a></li>
+			<li><a href="note_5.php">5. changes in net market values</a></li>
+			<li><a href="note_6.php">6. funding arrangmements</a></li>
+			<li><a href="note_7.php">7. income tax</a></li>
+			<li><a href="note_8.php">8. cash flow information</a></li>
+			<li><a href="note_9.php">9. sundry debtors</a></li>
+			<li><a href="note_10.php">10. sundry payables</a></li>
+			<li><a href="note_11.php">11. Auditor's remuneration</a></li>
+			<li><a href="note_12.php">12. liability for accrued benefits</a></li>
+			<li><a href="note_13.php">13. funds not allocated to member accounts</a></li>
+			<li><a href="note_14.php">14. guaranteed benefits</a></li>
+			<li><a href="note_15.php">15. vested benefits</a></li>
+			<li><a href="note_16.php">16. financial instruments</a></li>
+			<li><a href="note_17.php">17. related parties</a></li>
+			<li><a href="note_18.php">18. commitments, contingent liabilities and contingent assets</a></li>
+			<li><a href="note_19.php">19. subsequent events</a></li>
+		</ul>
+	</div>
---
+++ sections/financial-statements/Financial Statements PSSap/side-nav.php
@@ -0,0 +1,8 @@
+	<div class="col-xs-3 sidenav omega" id="sidenav">
+		<p class="sectiontitle">PSSap financial statement</p>
+		<h2><a id="top" title="Top of the page" name="top"></a>In this section</h2>
+		<ul>
+			<li><a href="financial-statement-pssap.php">PSSap financial statements</a></li>
+			<li><a href="fs_notes_landing.php">PSSap financial statement notes</a></li>
+		</ul>
+	</div>

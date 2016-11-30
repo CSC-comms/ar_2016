b88105b99a85b076bc567d665a99db528f6fbb3e
rabbit-bones <josmc82@gmail.com>
2016-11-30 13:30:50 +1100

    adding side nav files to replace in page navigation

--- sections/appendices/CSC-org-chart.php
+++ sections/appendices/CSC-org-chart.php
@@ -6,16 +6,8 @@
     
     <div class="container">
     <div class="row">
-    <div class="col-xs-3 sidenav omega" id="sidenav">
-	<p class="sectiontitle">Appendices</p>
-	<h2><a id="top" title="Top of the page" name="top"></a>In this section</h2>
-	<ul>
-        <li><a href="appendix1.php">Appendix 1</a></li>
-        <li><a href="">CSC organisational chart</a></li>
-        <li><a href="glossary.php">Glossary</a></li>
-        
-	</ul>
-		</div>
+	
+    <?php include="side-nav.php" ?>
         
         <div class="col-xs-8 content" id="startContent">        
 		<div class="content" id="startContent">
---
+++ sections/appendices/side-nav.php
@@ -0,0 +1,10 @@
+    <div class="col-xs-3 sidenav omega" id="sidenav">
+	<p class="sectiontitle">Appendices</p>
+	<h2><a id="top" title="Top of the page" name="top"></a>In this section</h2>
+	<ul>
+        <li><a href="appendix1.php">Appendix 1</a></li>
+        <li><a href="CSC-org-chart.php">CSC organisational chart</a></li>
+        <li><a href="glossary.php">Glossary</a></li>
+        
+	</ul>
+		</div>

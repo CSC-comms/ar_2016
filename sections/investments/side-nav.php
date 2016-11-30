483b702f5c16720cbe3d86f148012b6ff1343c30
rabbit-bones <josmc82@gmail.com>
2016-11-30 13:40:51 +1100

    replacing in page side nav with php include

---
+++ sections/investments/side-nav.php
@@ -0,0 +1,13 @@
+	<div class="col-xs-3 sidenav omega" id="sidenav">
+		<p class="sectiontitle">Investments</p>
+		<h2><a id="top" title="Top of the page" name="top"></a>In this section</h2>
+		<ul>
+			<li><a title="Investments" href="Introduction.php">Investments</a></li>
+			<li><a title="How performance affects a member's benefits" href="how-performance-affects.php">How performance affects a member's benefits</a></li>
+			<li><a title="Investment approach" href="investment-approach.php">Investment approach</a></li>
+			<li><a title="Investment strategy" href="investment-strategy.php">Investment strategy</a></li>
+			<li><a title="Investment governance" href="investment-governance.php">Investment governance</a></li>
+			<li><a title="Environmental, social and governance factors" href="environmental-factors.php">Environmental, social and governance factors</a></li>
+			<li><a title="Investment options" href="investment-options.php">Investment options</a></li>
+		</ul>
+	</div>

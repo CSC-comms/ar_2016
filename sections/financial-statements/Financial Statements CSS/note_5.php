f7d5a2ba64a5b5429873f5c647cc0bfe220a0031
rabbit-bones <josmc82@gmail.com>
2016-11-30 14:16:02 +1100

    replacing in page side nav with php include for css financials

--- sections/financial-statements/Financial Statements CSS/note_5.php
+++ sections/financial-statements/Financial Statements CSS/note_5.php
@@ -6,30 +6,8 @@
         
     <div class="container">
     <div class="row">
-    <div class="col-xs-3 sidenav omega" id="sidenav">
-	<p class="sectiontitle">CSS financial statement</p>
-	<h2><a id="top" title="Top of the page" name="top"></a>Notes</h2>
-	<ul>
-        <li><a href="note_1.php">1. description of the scheme</a></li>
-                <li><a href="note_2.php">2. basis of preparation</a></li>
-                <li><a href="note_3.php">3. summary of principle accounting policies</a></li>
-                <li><a href="note_4.php">4. investments</a></li>
-                <li><a href="note_5.php">5. changes in net market values</a></li>
-                <li><a href="note_6.php">6. funding arrangmements</a></li>
-                <li><a href="note_7.php">7. income tax</a></li>
-                <li><a href="note_8.php">8. sundry debtors</a></li>
-                <li><a href="note_9.php">9. transfers from the Commonwealth Superannuation Scheme to the Public Sector Superannuation Scheme</a></li>
-                <li><a href="note_10.php">10. operational risk reserve</a></li>
-                <li><a href="note_11.php">11. Auditor's remuneration</a></li>
-                <li><a href="note_12.php">12. unallocated time</a></li>
-                <li><a href="note_13.php">13. vested benefits</a></li>
-                <li><a href="note_14.php">14. liability for accrued benefits</a></li>
-                <li><a href="note_15.php">15. financial instruments</a></li>
-                <li><a href="note_16.php">16. related parties</a></li>
-                <li><a href="note_17.php">17. commitments, contingent liabilities and contingent assets</a></li>
-                <li><a href="note_18.php">18. subsequent events</a></li>
-	</ul>
-		</div>
+    
+		<?php include "side-nav-notes.php" ?>
         
 		<div class="col-xs-8 content" id="startContent">
 			<h1><?php echo $title ?></h1>

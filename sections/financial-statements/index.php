b62369d0a89dcd4859f9a73e91408551b38406b9
rabbit-bones <josmc82@gmail.com>
2016-12-02 14:49:42 +1100

    adding mobile responsive classes to fin statements

--- sections/financial-statements/index.php
+++ sections/financial-statements/index.php
@@ -5,7 +5,7 @@
 	?>
     <div class="container">
     <div class="row">
-    <div class="col-xs-3 sidenav omega" id="sidenav">
+    <div class="col-sm-3 sidenav omega" id="sidenav">
 	<p class="sectiontitle">Financial statements</p>
 	<h2><a id="top" title="Top of the page" name="top"></a>In this section</h2>
 	<ul>
@@ -13,7 +13,7 @@
 	</ul>
 		</div>
         
-		<div class="col-xs-8 content" id="startContent">
+		<div class="col-sm-8 col-xs-11 content" id="startContent">
 			<h1><?php echo $title ?></h1>
             
             <h2>Financial Statements as PDF</h2>
@@ -29,23 +29,19 @@
                 <li><a href="../../downloads/pdfs/09 CSC Auditor_s Report 2015-16.pdf">CSC Auditors report [PDF 1,005KB]</a></li>
                 <li><a href="../../downloads/pdfs/10 CSC Financial Statements 2015-16 Signed.pdf">CSC financial statements 2015-16 [PDF 341KB]</a></li>
             </ul>
-            
-            <!-- <h2>Financial Statements as accessible HTML</h2>
-            <p>Please note that the HTML statements do not include the Auditor's report, the Statement by the Trustee of Commonwealth Superannuation Corporation, or the Actuary report. These reports are provided in the PDF files listed above.
-            </p>
-            <ul>
-            	<li><a href="Financial Statements CSS/financial-statements-css.php">CSS financial statements 2015-16 [HTML]</a></li>
-                <li><a href="Financial Statements PSS/financial-statement-pss.php">PSS financial Statements 2015-16 [HTML]</a></li>
-                <li><a href="Financial Statements MS/financial-statement-ms.php">MilitarySuper financial statements 2015-16 [HTML]</a></li>
-                <li><a href="Financial Statements PSSap/financial-statement-pssap.php">PSSap financial statements 2015-16 [HTML]</a></li>
-                <li><a href="Financial Statements CSC/index.php">CSC financial statements 2015-16 [HTML]</a></li>          
-            </ul> -->
 
           
 		  
 	<ul class="bottomnav">
     <li><a href="../super-schemes/PNG.php" class="back">Previous &#8211; Super schemes: PNG</a></li><li><a href="#topnav" class="top">Top</a></li><li><a href="../appendices/appendix1.php" class="next">Next &#8211; Appendices</a></li>
 	</ul>
+	
+	<div class="btn-group" role="group">
+    <button type="button" class="btn btn-default"><a href="../super-schemes/PNG.php" class="back">Previous </a></button>
+	<button type="button" class="btn btn-default"><a href="#topnav" class="top">Top</a></button>
+	<button type="button" class="btn btn-default"><a href="../appendices/appendix1.php" class="next">Next</a></button>
+	</div>
+	
 </div>
 </div>
 </div>
